801b4d541110afcef734e0d0c3b04426669a97ea
LimJaeEun0930 <je9513666@gmail.com>
2024-11-26 15:40:49 +0900

    feat: 음식 주문받는 기능 구현

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -4,6 +4,9 @@ import static christmas.Constants.*;
 
 import camp.nextstep.edu.missionutils.Console;
 import christmas.exception.UnvalidDateException;
+import christmas.menu.IFood;
+import christmas.menu.MenuUtil;
+import java.util.HashMap;
 
 public class InputView {
 
@@ -17,9 +20,44 @@ public class InputView {
             } catch (IllegalArgumentException e) {
                 System.out.println("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
             }
-
         }
     }
+
+    public HashMap<IFood, Integer> getFoodOrder() {
+
+        while (true) {
+            try {
+                String input = input(INPUT_MESSAGE_ORDER_MENU);
+                return makeOrderDetails(input);
+            } catch (RuntimeException e) {
+                System.out.println(INPUT_MESSAGE_UNVALID_ORDER);
+            }
+        }
+    }
+
+    private HashMap<IFood, Integer> makeOrderDetails(String input) throws RuntimeException {
+        HashMap<IFood, Integer> orderDetails = new HashMap<>();
+        String[] orders = input.split(",");
+        for (String order : orders) {
+            String menuName = order.substring(0, order.indexOf('-'));
+            int count = Integer.parseInt(order.substring(order.indexOf('-') + 1));
+            IFood food = MenuUtil.getFoodByName(menuName);
+            makeHashMap(orderDetails, food, count);
+        }
+        return orderDetails;
+    }
+
+    private void makeHashMap(HashMap<IFood, Integer> orderDetail, IFood food, int count) throws IllegalArgumentException{
+        int sum = 0;
+        for (Integer foodCount : orderDetail.values()) {
+            sum += foodCount;
+        }
+        if (count < 1 || sum + count > 20 || orderDetail.containsKey(food)) {
+            throw new IllegalArgumentException();
+        }
+        orderDetail.put(food, count);
+    }
+
     private boolean isValidDate(int day) throws UnvalidDateException{
         if (1 <= day && 31 >= day) {
             return true;
